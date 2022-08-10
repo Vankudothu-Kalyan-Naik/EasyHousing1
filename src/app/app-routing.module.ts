f2bcd611fa330ee9d62e41e4af961e3a67c03efa
kalyan <kalyannaik938@gmail.com>
2022-08-10 21:00:09 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { BuyerLoginComponent } from './pages/buyer-login/buyer-login.component';
+import { BuyerRegisterComponent } from './pages/buyer-register/buyer-register.component';
+import { BuyerComponent } from './pages/buyer/buyer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LogindashbordComponent } from './pages/logindashbord/logindashbord.component';
+import { SellerLoginComponent } from './pages/seller-login/seller-login.component';
+import { SellerRegisterComponent } from './pages/seller-register/seller-register.component';
+import { SellerComponent } from './pages/seller/seller.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'login',component:LoginComponent},
+  {
+  path:'signup',component:SignupComponent
+  },
+  {path:'logindashbord',component:LogindashbordComponent},
+  {path:'buyer',component:BuyerComponent},
+  {path:'seller',component:SellerComponent},
+  {path:'buyer-login',component:BuyerLoginComponent},
+  {path:'buyer-register',component:BuyerRegisterComponent},
+  {path:'seller-login',component:SellerLoginComponent},
+  {path:'seller-register',component:SellerRegisterComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navpage/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LogindashbordComponent } from './pages/logindashbord/logindashbord.component';
+import { BuyerComponent } from './pages/buyer/buyer.component';
+import { SellerComponent } from './pages/seller/seller.component';
+import { BuyerLoginComponent } from './pages/buyer-login/buyer-login.component';
+import { BuyerRegisterComponent } from './pages/buyer-register/buyer-register.component';
+import { SellerLoginComponent } from './pages/seller-login/seller-login.component';
+import { SellerRegisterComponent } from './pages/seller-register/seller-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    SignupComponent,
+    LogindashbordComponent,
+    BuyerComponent,
+    SellerComponent,
+    BuyerLoginComponent,
+    BuyerRegisterComponent,
+    SellerLoginComponent,
+    SellerRegisterComponent
   ],
   imports: [
     BrowserModule,
